da7ed989ea072b884c007a17b880a9dbd6efdc55
Jonathan <kiritharan@cmu.edu>
2023-08-13 01:35:04 -0400

    before i comment to test count for each page of elems

--- extractpages.py
+++ extractpages.py
@@ -9,6 +9,7 @@ from PIL import Image
 
 
 
+
 last_img = 0  # last image that we cycled through when extracting multiple pages.
 dog = {}
 #receive images from layout object
@@ -55,18 +56,74 @@ for page_layout in extract_pages("WHEELING-Apr05-Wednesday-Afternoon-Program.pdf
     # each img starts around this y position 
     # 713,685,677,631,603,595,549,521,513,467,439,431,385,357,349,303,275,267,213,185,177,123,95,87
     #so first dog is defined between the confines of 713 and 685.
-
+# i don't think i want to loop through here. i'd rather go by number if it's all the same. 
+# and do that for each page. will b frustrating.
     print("going through page layout")
     for element in page_layout:
         # print(element)
         if isinstance(element, LTTextContainer):
             # print(vars(element))
+            print([element.y0, element.x0])
             print(element.get_text())
             # print(element.y1)
-            print(":)")
-        elif isinstance(element, LTFigure):
-            for e in element:
-                if isinstance(e, LTChar):
-                    print('this is the ltchar!!!!!!!!\n\n')
-                    text = element.get_text()
-                    # print(text)
+    
+"""
+[726.8025, 18.0] race title & distance 
+[719.6375, 140.33] # Race, date (Wednesday Afternoon April 05, 2023), bets (WPS, QUINIELA, $3 PERFECTA, TRIFECTA, & .10 SUPER ($2 MIN BET))
+
+ok the positions are not reliable.
+just go in order then?
+
+race(wheeling), #yards
+
+#race, date(Wednesday Afternoon April 05, 2023),bet,whateverthisis(FIRST HALF LUCKY NORTH TWIN TRI(FORCE OUT EVERY FRIDAY))
+
+wheeling, Grade
+
+firstdog weight (csr ##), dog info: (birth, parents, breeder)
+
+track info and something (not relevant), same
+
+next
+
+next
+
+next
+
+race history dog1:firstrace no date, second race no date(both in imgs), data up until weight for five races :)
+
+positions in lines all the way own
+
+far turn positions
+
+finish positions-- first digit and then then split on second and third to get lengths lost by 49
+38
+33
+612
+210
+37
+
+ART, speed figure, odds, grade of race
+
+dog 2 CSR
+
+track info WD, same
+
+next
+
+next
+
+next
+
+second dog info breeder and so on
+
+AFTER the 7th dog's finish position then we get the right side box for dog 1
+kennel, top row of unecessarything, bottom row of unecessary thing, trainer, weight and low and hi grade and best time, order of showing of first3 grayhounds(probably useless)
+
+chart writers comments on racing efforts for each race
+
+THEN that same info for all 7 dogs
+
+and then the dog 8's birthday
+might be a little weird 
+"""
